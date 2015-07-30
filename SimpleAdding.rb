152c35633b34d7afbdd7c7c654f59ed3dd509d2e
csh4626 <csh4626@naver.com>
2015-07-30 20:06:58 +0900

    Create SimpleAdding.rb

---
+++ SimpleAdding.rb
@@ -0,0 +1,11 @@
+def SimpleAdding(num)
+
+  # code goes here
+  return num+num*(0.5*(num-1))
+         
+end
+   
+# keep this function call here 
+# to see how to enter arguments in Ruby scroll down   
+SimpleAdding(STDIN.gets)  
+
